4b355895644d9628d1bd546c19005bc6e980dfa8
thatsymer <84711212+thatsymer@users.noreply.github.com>
2021-09-03 22:48:47 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const closeMenu= document.getElementById("nav-close");
+const openMenu= document.getElementById("nav-open");
+const navContainer = document.getElementById("nav-container")
+
+closeMenu.addEventListener("click", ()=>{
+    navContainer.classList.add('close')
+    navContainer.classList.remove('open')})
+
+openMenu.addEventListener("click", ()=>{
+    navContainer.classList.add('open')
+    navContainer.style.right = 0;
+    navContainer.classList.remove('close')
+})
